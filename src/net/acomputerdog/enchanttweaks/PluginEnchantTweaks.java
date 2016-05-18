9c096fae9b6fd64b61966ead7d2ab524323ac6c2
chris <acomputerdog@gmail.com>
2016-05-18 17:23:09 -0400

    Add support for unenchanting items

--- src/net/acomputerdog/enchanttweaks/PluginEnchantTweaks.java
+++ src/net/acomputerdog/enchanttweaks/PluginEnchantTweaks.java
@@ -1,6 +1,10 @@
 package net.acomputerdog.enchanttweaks;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -8,7 +12,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 public class PluginEnchantTweaks extends JavaPlugin implements Listener {
 
@@ -40,11 +48,67 @@ public class PluginEnchantTweaks extends JavaPlugin implements Listener {
             if (enchanter.getLevel() >= level) {
                 e.getItem().setType(Material.EXP_BOTTLE); //convert from glass to EXP bottles
                 e.getItem().setAmount(bottleCounts[e.whichButton()]); //set the amount of bottles
+                //TODO remove lapis, if it becomes possible at some point
 
-                //e.setExpLevelCost(xpLevels[e.whichButton()]); //set XP level to actually use the suggested amount
                 enchanter.setLevel(enchanter.getLevel() - level);
-                //e.setCancelled(false);
             }
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("unenchant")) {
+            if (sender.hasPermission("enchanttweaks.unenchant")) {
+                if (sender instanceof Player) {
+                    if (unEnchant((Player)sender)) {
+                        sendMessage(sender, "Item unenchanted successfully.");
+                    } else {
+                        sendError(sender, "Unable to unenchant item, make sure you hold it in your main hand or off hand.");
+                    }
+                } else {
+                    sendError(sender, "This command can only be run by a player!");
+                }
+            } else {
+                sendError(sender, "You do not have permission!");
+            }
+        } else {
+            sendError(sender, "Unknown command passed to SendAll!");
+        }
+        return true;
+    }
+
+    private void sendError(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.RED + message);
+    }
+
+    private void sendMessage(CommandSender sender, String message) {
+        sender.sendMessage(ChatColor.AQUA + message);
+    }
+
+    private boolean unEnchant(Player player) {
+        PlayerInventory inventory = player.getInventory();
+        ItemStack item = inventory.getItemInMainHand();
+        if (item == null) {
+            item = inventory.getItemInOffHand();
+        }
+        if (item != null) {
+            Map<Enchantment, Integer> enchantments = item.getEnchantments();
+            for (Map.Entry<Enchantment, Integer> enchant : enchantments.entrySet()) { //loop through each enchantment
+                ItemStack book = new ItemStack(Material.ENCHANTED_BOOK);
+
+                //enchanted books have to have enchantments stored through item meta
+                EnchantmentStorageMeta meta = (EnchantmentStorageMeta)book.getItemMeta();
+                meta.addStoredEnchant(enchant.getKey(), enchant.getValue(), true);
+                book.setItemMeta(meta);
+
+                if (!inventory.addItem(book).isEmpty()) { //add item and check if it fit
+                    player.getWorld().dropItem(player.getLocation(), book); //drop at feet if it didn't
+                }
+
+                item.removeEnchantment(enchant.getKey()); //remove the enchantment
+            }
+            return true;
+        }
+        return false;
     }
 }
